8a534e5c3ecf94d84844b323102192ab4c49988f
henrique86h <henriqueribeiromiguel86@gmail.com>
2023-09-05 21:56:38 -0300

    calculo de média

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ console.log("bem vindoo")
 alert("a média é:")
 
 alert((8+7+9+2)/4)
+
+// o sinal de "igual" ou "=" é um operador de atribuição
+
+nota1 = 10
+nota2 = 8
+nota3 = 5
+nota4 = 8
+
+console.log (" A média do João é")
+//console.log ((nota1 + nota2 + nota2 + nota4)/4)
+
+media = (nota1 + nota2 + nota2 + nota4)/4
+
+console.log(media)
